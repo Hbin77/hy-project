42d684d87d66cf5554813bc535cd36febd5363c7
hbin <phb007298@gmail.com>
2024-05-22 19:17:01 +0900

    sundae-commit

---
+++ 데코.py
@@ -0,0 +1,34 @@
+# b = 7000
+# p = 12000
+# s = 9000
+
+# b_count = int(input("주문할 햄버거 수량"))
+# p_count = int(input("주문할 피자 수량"))
+# s_count = int(input("주문할 스파게티 수량"))
+# people = int(input("인원 수"))
+
+# prize = b*b_count+p*p_count+s*s_count
+
+# oneprize = int(prize/people)
+
+# print("총 주문 가격은 ",prize,"원 입니다. 한명 당 내야 할 금액은 ", oneprize,"원 입니다.")
+
+# score=int(input("enter your score:"))
+
+# if score>90 :
+    
+# elif score>80 :
+    
+# elif score>80 :
+    
+# elif score>80 :
+    
+    
+    
+    
+n = int(input())
+
+for i in range(1, n+1) :
+    for j in range(1, i+1):
+        print(j, end='')   
+    print()
